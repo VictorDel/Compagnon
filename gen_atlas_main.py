db1b03769ce2bc064fb54325adaff779e48e93f4
Victor DELATTRE <vd239549@is152845.intra.cea.fr>
2016-10-25 18:31:38 +0200

    doc

--- gen_atlas_main.py
+++ gen_atlas_main.py
@@ -12,8 +12,8 @@ at_dir = '/neurospin/grip/protocols/MRI/Resting_state_Victor_2014/atlases/test_g
 roi_thresh = 0.5 #Threshold of voxel ratio for roi to be considered 
 #if number of voxel in individual brain mask and roi of reference atlas / number of voxel in roi of reference atlas < threshold then roi will be empty
 brain_mask_thresh = 0.1 #Threshold for mask binarization (times average intensity of mask : 0.1 means 10% of average mask intensity)
-savedir = 'in_subjdir'
-namelog='patients'
+savedir = 'in_subjdir'# where will the atlases be save 'in_subjdir' means default dir where anats are if 'other' they are saved in at_dir
+namelog='patients' #log will be saved in root
 
 
 gen_atlas_indiv_function.gen_atlas_indiv(atlas_template,labels,root,basename,savedir,at_dir
